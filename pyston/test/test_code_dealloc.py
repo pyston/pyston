83962497971bf66ecc28c16faaba6b74659f74c6
Kevin Modzelewski <kevmod@gmail.com>
2021-03-15 14:54:27 -0400

    test to make sure we free code objects

---
+++ pyston/test/test_code_dealloc.py
@@ -0,0 +1,17 @@
+import gc
+import resource
+
+if __name__ == "__main__":
+    resource.setrlimit(resource.RLIMIT_AS, (2<<20, 2<<20))
+
+    for i in range(100000):
+        # The exec creates a code object, but we also want to create and call a function
+        # so that we can test the zombieframe behavior
+        exec("""
+def f():
+    pass
+f()
+# import sys; sys.path.append(f.__code__)
+""")
+        if i % 100 == 0:
+            gc.collect()
