cc5444981208f3226c55fdb8785e62d2591d8a3e
Marius Wachtler <undingen@gmail.com>
2021-05-04 10:17:35 +0200

    add script to benchmark release packages This script runs our benchmarks and pyperformance on all supported distros. It will save the benchmark results and display a summary. Idea is that this script makes it easier to spot a problem with a release because they should all have similar performance also it makes sure that every package can at least run the benchmarks without problems.

---
+++ pyston/tools/bench_release.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+set -e
+VERSION=2.2
+INPUT_DIR=${PWD}/release/${VERSION}
+OUTPUT_DIR=`realpath ${INPUT_DIR}/bench`
+
+if [ -d $OUTPUT_DIR ]
+then
+    echo "Directory $OUTPUT_DIR already exists";
+    exit 1
+fi
+mkdir -p $OUTPUT_DIR
+
+for DIST in 16.04 18.04 20.04
+do
+    echo "Benchmarking $DIST release"
+    # mount input dir readonly to make sure we are not accidently modifying it
+    docker run -iv${INPUT_DIR}:/host-volume-in:ro -iv${OUTPUT_DIR}:/host-volume-out --rm ubuntu:$DIST sh -s <<EOF
+set -ex
+export DEBIAN_FRONTEND=noninteractive
+apt-get update
+apt-get upgrade -y
+apt-get install -y build-essential git time libffi-dev
+# pillow deps:
+apt-get install -y libtiff5-dev libjpeg8-dev libopenjp2-7-dev zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev tcl8.6-dev tk8.6-dev python3-tk libharfbuzz-dev libfribidi-dev libxcb1-dev
+
+git clone https://github.com/pyston/python-macrobenchmarks.git
+# disble pytorch test
+sed -i 's/pytorch_alexnet_inference//g' python-macrobenchmarks/run_all.sh
+
+# pyston deb package
+apt-get install -y /host-volume-in/pyston_${VERSION}_${DIST}.deb
+pyston -mpip install pyperformance virtualenv
+pyston -mpyperformance run -f -o /host-volume-out/pyston_${VERSION}_${DIST}.json
+chown -R $(id -u):$(id -g) /host-volume-out/pyston_${VERSION}_${DIST}.json
+
+python-macrobenchmarks/run_all.sh /usr/bin/pyston
+mv results /host-volume-out/results_pyston_${DIST}
+chown -R $(id -u):$(id -g) /host-volume-out/results_pyston_${DIST}
+
+# cpython
+# some benchmarks don't run on python 3.5/3.6 so only run this benchmarks on 3.8 (20.04)
+if [ "$DIST" = "20.04" ]; then
+    apt-get install -y python3 python3-dev python3-pip python3-venv
+    pyston -mpyperformance run -f -p /usr/bin/python3 -o /host-volume-out/cpython_${DIST}.json
+    chown -R $(id -u):$(id -g) /host-volume-out/cpython_${DIST}.json
+    pyston -mpyperformance compare -O table /host-volume-out/cpython_${DIST}.json /host-volume-out/pyston_${VERSION}_${DIST}.json > /host-volume-out/pyperformance_diff_${DIST}.txt
+    chown -R $(id -u):$(id -g) /host-volume-out/pyperformance_diff_${DIST}.txt
+    
+    python-macrobenchmarks/run_all.sh /usr/bin/python3
+    mv results /host-volume-out/results_cpython_${DIST}
+    chown -R $(id -u):$(id -g) /host-volume-out/results_cpython_${DIST}
+fi
+EOF
+done
+
+echo "FINISHED: wrote benchmark results to $OUTPUT_DIR"
+
+# display a summary of the benchmark results:
+cat ${OUTPUT_DIR}/pyperformance_diff_20.04.txt
+
+for f in ${OUTPUT_DIR}/results_pyston_20.04/*.out
+do
+    echo "benchmark: `basename $f`"
+    for DIST in 16.04 18.04 20.04
+    do
+        f_pyston=${f/20.04/$DIST}
+        f_cpython=${f_pyston/results_pyston/results_cpython}
+        t_pyston=`cat $f_pyston | grep 'User time' | cut -d' ' -f4`
+        if [ -f "$f_cpython" ]; then
+            t_cpython=`cat $f_cpython | grep 'User time' | cut -d' ' -f4`
+            echo "  $DIST# pyston: $t_pyston cpython: $t_cpython"
+	else
+            echo "  $DIST# pyston: $t_pyston"
+        fi
+    done
+done
+
