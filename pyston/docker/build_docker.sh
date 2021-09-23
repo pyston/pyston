7ea9d441a6b01b8fd28fe60495238f4805e29483
Kevin Modzelewski <kevmod@gmail.com>
2021-09-23 14:26:09 -0400

    Build a v2.3.1 docker container

--- pyston/docker/build_docker.sh
+++ pyston/docker/build_docker.sh
@@ -1,7 +1,7 @@
 set -eux
 
-BUILD_NAME=2.3
+BUILD_NAME=2.3.1
 
-docker build -t pyston/pyston:latest -t pyston/pyston:${BUILD_NAME} .
+docker build -t pyston/pyston:latest -t pyston/pyston:${BUILD_NAME} $(dirname $0)
 docker push pyston/pyston:latest
 docker push pyston/pyston:${BUILD_NAME}
