d0f838f6e7f471afde433dedcb0821832eac09bd
Marius Wachtler <undingen@gmail.com>
2021-10-05 14:08:36 +0200

    conda: don't hardcode version numbers in scripts

--- pyston/conda/pyston/build_base.sh
+++ pyston/conda/pyston/build_base.sh
@@ -25,17 +25,17 @@ make -j`nproc` pyston3
 
 OUTDIR=$SRC_DIR/build/opt_install
 
-cp $OUTDIR/usr/bin/python3.bolt ${PREFIX}/bin/python3.8-pyston2.3
-ln -s ${PREFIX}/bin/python3.8-pyston2.3 ${PREFIX}/bin/pyston
-ln -s ${PREFIX}/bin/python3.8-pyston2.3 ${PREFIX}/bin/pyston3
+cp $OUTDIR/usr/bin/python3.bolt ${PREFIX}/bin/python${PYTHON_VERSION2}-pyston${PYSTON_VERSION2}
+ln -s ${PREFIX}/bin/python${PYTHON_VERSION2}-pyston${PYSTON_VERSION2} ${PREFIX}/bin/pyston
+ln -s ${PREFIX}/bin/python${PYTHON_VERSION2}-pyston${PYSTON_VERSION2} ${PREFIX}/bin/pyston3
 
 cp -r $OUTDIR/usr/include/* ${PREFIX}/include/
 cp -r $OUTDIR/usr/lib/* ${PREFIX}/lib/
 
 # remove pip
-rm -r ${PREFIX}/lib/python3.8-pyston2.3/site-packages/pip*
+rm -r ${PREFIX}/lib/python${PYTHON_VERSION2}-pyston${PYSTON_VERSION2}/site-packages/pip*
 
 # move site-packages directory to cpythons default site-package directory and create a symlink from pyston location
-mkdir ${PREFIX}/lib/python3.8/
-mv ${PREFIX}/lib/python3.8-pyston2.3/site-packages ${PREFIX}/lib/python3.8/
-ln -s ${PREFIX}/lib/python3.8/site-packages/ ${PREFIX}/lib/python3.8-pyston2.3/site-packages
+mkdir ${PREFIX}/lib/python${PYTHON_VERSION2}/
+mv ${PREFIX}/lib/python${PYTHON_VERSION2}-pyston${PYSTON_VERSION2}/site-packages ${PREFIX}/lib/python${PYTHON_VERSION2}/
+ln -s ${PREFIX}/lib/python${PYTHON_VERSION2}/site-packages/ ${PREFIX}/lib/python${PYTHON_VERSION2}-pyston${PYSTON_VERSION2}/site-packages
--- pyston/conda/python/build_base.sh
+++ pyston/conda/python/build_base.sh
@@ -2,5 +2,5 @@
 set -eux
 
 # replace python
-ln -sf ${PREFIX}/bin/python3.8-pyston2.3 ${PREFIX}/bin/python
-ln -sf ${PREFIX}/bin/python3.8-pyston2.3 ${PREFIX}/bin/python3
+ln -sf ${PREFIX}/bin/python${PYTHON_VERSION2}-pyston${PYSTON_VERSION2} ${PREFIX}/bin/python
+ln -sf ${PREFIX}/bin/python${PYTHON_VERSION2}-pyston${PYSTON_VERSION2} ${PREFIX}/bin/python3
