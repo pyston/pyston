e0d5a2f5c4a9fcdd05630a2d77f1014697a55013
Marius Wachtler <undingen@gmail.com>
2021-10-06 14:02:12 +0200

    conda: add python3.8 symlink

--- pyston/conda/python/build_base.sh
+++ pyston/conda/python/build_base.sh
@@ -4,3 +4,4 @@ set -eux
 # replace python
 ln -sf ${PREFIX}/bin/python${PYTHON_VERSION2}-pyston${PYSTON_VERSION2} ${PREFIX}/bin/python
 ln -sf ${PREFIX}/bin/python${PYTHON_VERSION2}-pyston${PYSTON_VERSION2} ${PREFIX}/bin/python3
+ln -sf ${PREFIX}/bin/python${PYTHON_VERSION2}-pyston${PYSTON_VERSION2} ${PREFIX}/bin/python${PYTHON_VERSION2}
