99a0dfb0de3a566edfad121ac9151eabf3d6636f
Pierre-Louis Peeters <pierre-louis.peeters@bisnode.com>
2021-10-18 15:05:46 +0200

    Add Dockerfile for build based on bullseye-slim

--- pyston/docker/build_docker.sh
+++ pyston/docker/build_docker.sh
@@ -1,7 +1,16 @@
+#!/bin/sh
+
 set -eux
 
 BUILD_NAME=2.3.1
+DIR=$(dirname $0)
 
-docker build -t pyston/pyston:latest -t pyston/pyston:${BUILD_NAME} $(dirname $0)
+docker build -t pyston/pyston:latest -t pyston/pyston:${BUILD_NAME} -f ${DIR}/Dockerfile ${DIR}
 docker push pyston/pyston:latest
 docker push pyston/pyston:${BUILD_NAME}
+
+docker build -t pyston/pyston:slim-bullseye -t pyston/pyston:slim -t pyston/pyston:${BUILD_NAME}-slim-bullseye -t pyston/pyston:${BUILD_NAME}-slim -f ${DIR}/Dockerfile.slim-bullseye ${DIR}
+docker push pyston/pyston:slim
+docker push pyston/pyston:slim-bullseye
+docker push pyston/pyston:${BUILD_NAME}-slim-bullseye
+docker push pyston/pyston:${BUILD_NAME}-slim
