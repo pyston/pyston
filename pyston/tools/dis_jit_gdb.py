3b287f90411bbb01b25eec0c6f92fdf3f377c003
Marius Wachtler <undingen@gmail.com>
2022-02-15 19:54:56 +0100

    perf add ARM64 support and update dis_jit_gdb script

--- pyston/tools/dis_jit_gdb.py
+++ pyston/tools/dis_jit_gdb.py
@@ -17,18 +17,20 @@ def stop_handler(event):
 
     gdb.execute("frame 1")
     co = gdb.parse_and_eval("co")
-    print("Dissasembly of", gdb.parse_and_eval("PyUnicode_AsUTF8(co->co_name)"))
+    def getPythonStr(cmd):
+        return str(gdb.parse_and_eval(f"PyUnicode_AsUTF8({cmd})")).split('"')[1]
+    print(f"Dissasembly of {getPythonStr('co->co_filename')}:{int(gdb.parse_and_eval('co->co_firstlineno'))} {getPythonStr('co->co_name')}")
 
-    num_opcodes = int(gdb.parse_and_eval("num_opcodes"))
+    num_opcodes = int(gdb.parse_and_eval("jit.num_opcodes"))
     for i in range(num_opcodes):
-        codeunit = int(gdb.parse_and_eval(f"first_instr[{i}]"))
+        codeunit = int(gdb.parse_and_eval(f"jit.first_instr[{i}]"))
         opcode = codeunit & 0xFF
         oparg = codeunit >> 8
         print(f"{i*2:8} {dis.opname[opcode]:30} {oparg:10}")
 
         flags = "" # "/r" to see machine code bytes too
-        until = f"opcode_addr_begin[{i}+1]" if i+1 < num_opcodes else "labels[lbl_epilog]"
-        gdb.execute(f"disassemble {flags} opcode_addr_begin[{i}], {until}")
+        until = f"((int*)(labels[lbl_opcode_addr_begin]))[{i}+1]" if i+1 < num_opcodes else "labels[lbl_epilog]"
+        gdb.execute(f"disassemble {flags} ((int*)(labels[lbl_opcode_addr_begin]))[{i}], {until}")
 
 gdb.events.stop.connect(stop_handler)
 gdb.Breakpoint("dasm_free")
