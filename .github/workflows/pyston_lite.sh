1152028e71f107702ce81fb53576b008160c4695
Kevin Modzelewski <kevmod@gmail.com>
2022-05-19 18:57:45 -0400

    Add CI test for pyston_lite

---
+++ .github/workflows/pyston_lite.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -eux
+
+# workaround for setuptools 60
+export SETUPTOOLS_USE_DISTUTILS=stdlib
+
+export DEBIAN_FRONTEND=noninteractive
+
+# install dependencies
+sudo apt-get update
+
+sudo --preserve-env=DEBIAN_FRONTEND apt-get install -y build-essential python3.8-full python3.8-dev libpython3.8-testsuite virtualenv luajit
+
+sudo chown -R `whoami` /pyston_dir
+
+cd /pyston_dir/pyston/pyston_lite
+
+make test -j$(nproc)
