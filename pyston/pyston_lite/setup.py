a85963998bbb452a9fa0ca08fb48e8fde057e9de
Kevin Modzelewski <kevmod@gmail.com>
2022-05-23 21:11:37 -0400

    add bolt to pyston-lite

--- pyston/pyston_lite/setup.py
+++ pyston/pyston_lite/setup.py
@@ -1,11 +1,20 @@
 from distutils.core import setup, Extension, Distribution
 from distutils.command.build_ext import build_ext
 from distutils import sysconfig
+import glob
 import os
 import shutil
 import subprocess
 import sys
 import tempfile
+
+def check_call(args, **kw):
+    print("check_call", " ".join([repr(a) for a in args]), kw)
+    return subprocess.check_call(args, **kw)
+
+def check_output(args, **kw):
+    print("check_output", " ".join([repr(a) for a in args]), kw)
+    return subprocess.check_output(args, **kw)
 
 class pyston_build_ext(build_ext):
     def build_extension(self, ext):
@@ -33,7 +42,7 @@ class pyston_build_ext(build_ext):
             # Step 2, run pgo task:
             with tempfile.TemporaryDirectory() as dir:
                 envdir = os.path.join(dir, "pgo_env")
-                subprocess.check_call([sys.executable, "-m", "venv", envdir])
+                check_call([sys.executable, "-m", "venv", envdir])
 
                 env_python = os.path.join(envdir, "bin/python3")
 
@@ -41,7 +50,7 @@ class pyston_build_ext(build_ext):
                 shutil.copy(self.get_ext_fullpath(ext.name), site_packages)
                 shutil.copy(os.path.join(os.path.dirname(__file__), "autoload/pyston_lite_autoload.pth"), site_packages)
 
-                subprocess.check_call([env_python, "-c", "import sys; assert 'pyston_lite' in sys.modules"])
+                check_call([env_python, "-c", "import sys; assert 'pyston_lite' in sys.modules"])
 
                 parallel = "-j0"
                 if subprocess.check_output(["uname", "-m"]).decode("utf8").strip() == "aarch64":
@@ -62,6 +71,31 @@ class pyston_build_ext(build_ext):
             ext.extra_compile_args = extra_args
             ext.extra_link_args = extra_link_args
 
+
+        if "NOBOLT" not in os.environ:
+            with tempfile.TemporaryDirectory() as dir:
+                envdir = os.path.join(dir, "bolt_env")
+                check_call([sys.executable, "-m", "venv", envdir])
+
+                env_python = os.path.join(envdir, "bin/python3")
+
+                site_packages = check_output([env_python, "-c", "import site; print(site.getsitepackages()[0])"]).decode("utf8").strip()
+
+                shutil.copy(os.path.join(os.path.dirname(__file__), "autoload/pyston_lite_autoload.pth"), site_packages)
+
+                ext_name = self.get_ext_fullpath(ext.name)
+                os.rename(ext_name, ext_name + ".prebolt")
+                install_extname = os.path.join(site_packages, os.path.basename(ext_name))
+                check_call(["../../build/bolt/bin/llvm-bolt", ext_name + ".prebolt", "-instrument", "-instrumentation-file-append-pid", "-instrumentation-file=" + install_extname, "-o", install_extname, "-skip-funcs=_PyEval_EvalFrameDefault,_PyEval_EvalFrame_AOT_Interpreter.*"])
+
+
+                subprocess.call([env_python, os.path.join(os.path.dirname(__file__), "../../Lib/test/regrtest.py"), "-j0", "-unone,decimal", "-x", *"test_posix test_asyncio test_cmd_line_script test_compiler test_concurrent_futures test_ctypes test_dbm_dumb test_dbm_ndbm test_distutils test_ensurepip test_ftplib test_gdb test_httplib test_imaplib test_ioctl test_linuxaudiodev test_multiprocessing test_nntplib test_ossaudiodev test_poplib test_pydoc test_signal test_socket test_socketserver test_ssl test_subprocess test_sundry test_thread test_threaded_import test_threadedtempfile test_threading test_threading_local test_threadsignals test_venv test_zipimport_support test_code test_capi".split()])
+
+                check_call(["../../build/bolt/bin/merge-fdata"] + glob.glob(install_extname + ".*.fdata"), stdout=open(install_extname + ".fdata", "wb"))
+
+                check_call(["../../build/bolt/bin/llvm-bolt", ext_name + ".prebolt", "-o", ext_name, "-data=" + install_extname + ".fdata", "-update-debug-sections", "-reorder-blocks=cache+", "-reorder-functions=hfsort+", "-split-functions=3", "-icf=1", "-inline-all", "-split-eh", "-reorder-functions-use-hot-size", "-peepholes=all", "-jump-tables=aggressive", "-inline-ap", "-indirect-call-promotion=all", "-dyno-stats", "-use-gnu-stack", "-jump-tables=none", "-frame-opt=hot"])
+                os.unlink(ext_name + ".prebolt")
+
         del self.compiler._compile
 
     def run(self):
@@ -75,8 +109,8 @@ ext = Extension(
         sources=["aot_ceval.c", "aot_ceval_jit.gen.c", "aot_ceval_jit_helper.c", "lib.c"],
         include_dirs=["../../pyston/LuaJIT", os.path.join(sysconfig.get_python_inc(), "internal")],
         define_macros=[("PYSTON_LITE", None), ("PYSTON_SPEEDUPS", "1"), ("Py_BUILD_CORE", None), ("ENABLE_AOT", None)],
-        extra_compile_args=["-std=gnu99", "-flto", "-fuse-linker-plugin", "-ffat-lto-objects", "-flto-partition=none", "-fno-semantic-interposition", "-specs=../tools/no-pie-compile.specs"],
-        extra_link_args=["-flto", "-fuse-linker-plugin", "-ffat-lto-objects", "-flto-partition=none", "-fno-semantic-interposition", "-specs=../tools/no-pie-link.specs"],
+        extra_compile_args=["-std=gnu99", "-flto", "-fuse-linker-plugin", "-ffat-lto-objects", "-flto-partition=none", "-fno-semantic-interposition", "-specs=../tools/no-pie-compile.specs", "-fno-reorder-blocks-and-partition"],
+        extra_link_args=["-flto", "-fuse-linker-plugin", "-ffat-lto-objects", "-flto-partition=none", "-fno-semantic-interposition", "-specs=../tools/no-pie-link.specs", "-Wl,--emit-relocs"],
 )
 
 setup(name="pyston_lite",
