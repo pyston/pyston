c187f451b8fd2ffca0e27b5c422b43ad9fbdd339
Kevin Modzelewski <kevmod@gmail.com>
2022-05-23 20:05:49 -0400

    Disable pgo for the qemu CI build
    I didn't test it but I think it would probably take a very long time;
    the qemu build already takes over an hour, and pgo multiplies the
    build time by a couple times.

--- pyston/pyston_lite/setup.py
+++ pyston/pyston_lite/setup.py
@@ -22,35 +22,42 @@ class pyston_build_ext(build_ext):
             return orig_compile_func(obj, src, ext, cc_args, extra_postargs, pp_opts)
         self.compiler._compile = new_compile
 
+        if "NOPGO" in os.environ:
+            super(pyston_build_ext, self).build_extension(ext)
+        else:
+            # Step 1, build with instrumentation:
+            ext.extra_compile_args = extra_args + ["-fprofile-generate"]
+            ext.extra_link_args = extra_link_args + ["-fprofile-generate"]
+            super(pyston_build_ext, self).build_extension(ext)
 
-        ext.extra_compile_args = extra_args + ["-fprofile-generate"]
-        ext.extra_link_args = extra_link_args + ["-fprofile-generate"]
-        super(pyston_build_ext, self).build_extension(ext)
+            # Step 2, run pgo task:
+            with tempfile.TemporaryDirectory() as dir:
+                envdir = os.path.join(dir, "pgo_env")
+                subprocess.check_call([sys.executable, "-m", "venv", envdir])
 
-        with tempfile.TemporaryDirectory() as dir:
-            envdir = os.path.join(dir, "pgo_env")
-            subprocess.check_call([sys.executable, "-m", "venv", envdir])
+                env_python = os.path.join(envdir, "bin/python3")
 
-            env_python = os.path.join(envdir, "bin/python3")
+                site_packages = subprocess.check_output([env_python, "-c", "import site; print(site.getsitepackages()[0])"]).decode("utf8").strip()
+                shutil.copy(self.get_ext_fullpath(ext.name), site_packages)
+                shutil.copy(os.path.join(os.path.dirname(__file__), "autoload/pyston_lite_autoload.pth"), site_packages)
 
-            site_packages = subprocess.check_output([env_python, "-c", "import site; print(site.getsitepackages()[0])"]).decode("utf8").strip()
-            shutil.copy(self.get_ext_fullpath(ext.name), site_packages)
-            shutil.copy(os.path.join(os.path.dirname(__file__), "autoload/pyston_lite_autoload.pth"), site_packages)
+                subprocess.check_call([env_python, "-c", "import sys; assert 'pyston_lite' in sys.modules"])
 
-            subprocess.check_call([env_python, "-c", "import sys; assert 'pyston_lite' in sys.modules"])
+                subprocess.call([env_python, os.path.join(os.path.dirname(__file__), "../../Lib/test/regrtest.py"), "-unone,decimal", "-x", *"test_posix test_asyncio test_cmd_line_script test_compiler test_concurrent_futures test_ctypes test_dbm_dumb test_dbm_ndbm test_distutils test_ensurepip test_ftplib test_gdb test_httplib test_imaplib test_ioctl test_linuxaudiodev test_multiprocessing test_nntplib test_ossaudiodev test_poplib test_pydoc test_signal test_socket test_socketserver test_ssl test_subprocess test_sundry test_thread test_threaded_import test_threadedtempfile test_threading test_threading_local test_threadsignals test_venv test_zipimport_support test_code test_capi".split()])
 
-            subprocess.call([env_python, os.path.join(os.path.dirname(__file__), "../../Lib/test/regrtest.py"), "-unone,decimal", "-x", *"test_posix test_asyncio test_cmd_line_script test_compiler test_concurrent_futures test_ctypes test_dbm_dumb test_dbm_ndbm test_distutils test_ensurepip test_ftplib test_gdb test_httplib test_imaplib test_ioctl test_linuxaudiodev test_multiprocessing test_nntplib test_ossaudiodev test_poplib test_pydoc test_signal test_socket test_socketserver test_ssl test_subprocess test_sundry test_thread test_threaded_import test_threadedtempfile test_threading test_threading_local test_threadsignals test_venv test_zipimport_support test_code test_capi".split()])
+            # Step 3, recompile using profiling data:
 
-        # Force recompilation:
-        # (we could also try to clean things up but that seems more difficult)
-        self.force = True
+            # Force recompilation:
+            # (we could also try to clean things up but that seems more difficult)
+            self.force = True
 
-        ext.extra_compile_args = extra_args + ["-fprofile-use"]
-        ext.extra_link_args = extra_link_args + ["-fprofile-use"]
-        super(pyston_build_ext, self).build_extension(ext)
+            ext.extra_compile_args = extra_args + ["-fprofile-use"]
+            ext.extra_link_args = extra_link_args + ["-fprofile-use"]
+            super(pyston_build_ext, self).build_extension(ext)
 
-        ext.extra_compile_args = extra_args
-        ext.extra_link_args = extra_link_args
+            ext.extra_compile_args = extra_args
+            ext.extra_link_args = extra_link_args
+
         del self.compiler._compile
 
     def run(self):
