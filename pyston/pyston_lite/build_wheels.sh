83097c19bd75640017b4e96f98dc1152074e3603
Kevin Modzelewski <kevmod@gmail.com>
2022-06-03 20:38:27 +0000

    manylinux: compile luajit from source
    looks like there's not a distro-provided package for it for arm

--- pyston/pyston_lite/build_wheels.sh
+++ pyston/pyston_lite/build_wheels.sh
@@ -4,7 +4,9 @@ cd $(dirname $0)
 
 PYTHON=/opt/python/cp38-cp38/bin/python3
 
-yum install -y luajit
+make -C ../LuaJIT -j`nproc`
+make -C ../LuaJIT -j`nproc` install
+ln -sf luajit-2.1.0-beta3 /usr/local/bin/luajit
 
 # manylinux2014 doesn't come with a python3 symlink:
 if ! $(which python3) ; then
