2252270d7806b7145e3783af1258f08c9e26e3ae
Kevin Modzelewski <kevmod@gmail.com>
2022-06-03 14:56:22 -0400

    lite: run bolt outside of the manylinux container
    The bolt that has been built on the host does not typically work
    inside the manylinux container. We could try to build bolt inside
    the container, or maybe we could create a package and install it.
    
    But it seemed easier to just generate a non-bolted wheel, then
    run bolt on the host and repackage the wheel.

--- pyston/pyston_lite/setup.py
+++ pyston/pyston_lite/setup.py
@@ -11,6 +11,7 @@ import tempfile
 NOBOLT = "NOBOLT" in os.environ or sys.platform == "darwin"
 NOLTO = "NOLTO" in os.environ or sys.platform == "darwin"
 NOPGO = "NOPGO" in os.environ
+BOLTFLAGS = "BOLTFLAGS" in os.environ
 
 def check_call(args, **kw):
     print("check_call", " ".join([repr(a) for a in args]), kw)
@@ -114,7 +115,7 @@ def get_cflags():
     flags = ["-std=gnu99", "-fno-semantic-interposition", "-specs=../tools/no-pie-compile.specs"]
     if not NOLTO:
         flags += ["-flto", "-fuse-linker-plugin", "-ffat-lto-objects", "-flto-partition=none"]
-    if not NOBOLT:
+    if not NOBOLT or BOLTFLAGS:
         flags += ["-fno-reorder-blocks-and-partition"]
     return flags
 
@@ -122,7 +123,7 @@ def get_ldflags():
     flags = ["-fno-semantic-interposition", "-specs=../tools/no-pie-link.specs"]
     if not NOLTO:
         flags += ["-flto", "-fuse-linker-plugin", "-ffat-lto-objects", "-flto-partition=none"]
-    if not NOBOLT:
+    if not NOBOLT or BOLTFLAGS:
         flags += ["-Wl,--emit-relocs"]
     return flags
 
